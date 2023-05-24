29c29025b1bce66d4b7ebe89ee83addc441e80f9
maxlazaro13 <maxlazaromax@gmail.com>
2023-05-24 16:12:18 -0300

    exercicio dois concluido exercicio três concluido

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,3 +26,45 @@ import java.util.Scanner
 
 //---------------------------------------------------------------------------------------------------------------------
 
+//2 - Escreva um programa que peça ao usuário para digitar um número inteiro e determine se ele é positivo ou negativo
+
+// fun main(args: Array<String>){
+//     exercio2()
+// }
+//
+//fun exercio2(){
+//    println("digite um numero")
+//    var scan = Scanner(System.`in`)
+//    var numero1 = scan.nextInt()
+//
+//    if (numero1 > 0){
+//        println("positivo")
+//    }else {
+//        println("negativo")
+//    }
+//}
+
+//--------------------------------------------------------------------------------------------------------------------
+
+//3 - Escreva um programa que peça ao usuário para digitar sua idade e determine se ele é maior ou menor de idade.
+
+//fun main(args: Array<String>){
+//    exercicio3()
+//}
+//
+//fun exercicio3(){
+//    println("digite uma idade")
+//    var scan = Scanner(System.`in`)
+//    var idade = scan.nextInt()
+//
+//    if (idade > 17){
+//        println("maior idade")
+//    }else {
+//        println("menor idade+")
+//    }
+//}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
